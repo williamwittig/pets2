1b3d5b6150e03df17e9f10fd035176ffd5392795
Renee Holter <holter.renee@student.greenriver.edu>
2022-05-05 13:01:19 -0400

    added changes to something

--- index.php
+++ index.php
@@ -22,6 +22,11 @@ $f3->route('GET /order', function() {
     $view = new Template();
     echo $view->render('views/pet-order.html');
 });
+$f3->route('POST /order2', function() {
+    var_dump($_POST);
+    $view = new Template();
+    echo $view->render('views/pet-order2.html');
+});
 
 // Run fat free
 $f3->run();
