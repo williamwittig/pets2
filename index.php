eab2095133c20ff173e21329430c12592a117ff5
williamwittig2 <wwittig@student.greenriver.edu>
2022-05-05 13:15:48 -0400

    Added summary page

--- index.php
+++ index.php
@@ -22,10 +22,24 @@ $f3->route('GET /order', function() {
     $view = new Template();
     echo $view->render('views/pet-order.html');
 });
+
 $f3->route('POST /order2', function() {
     var_dump($_POST);
+
+    $_SESSION['petType'] = $_POST['petType'];
+    $_SESSION['inputColor'] = $_POST['inputColor'];
+
     $view = new Template();
     echo $view->render('views/pet-order2.html');
+});
+
+$f3->route('POST /summary', function() {
+    var_dump($_POST);
+
+    $_SESSION['petName'] = $_POST['petName'];
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 // Run fat free
